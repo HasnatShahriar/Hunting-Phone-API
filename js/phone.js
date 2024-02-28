9ba50eeb83a7a386d589f93c8c17c996aa4f8e0b
HasnatShahriar <shahriarhriday94@gmail.com>
2024-02-29 00:55:25 +0600

    fullfill the modal part

--- js/phone.js
+++ js/phone.js
@@ -1,16 +1,16 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText = '13', isShowAll) => {
   const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
   const data = await response.json()
   const phones = data.data
   // console.log(phone)
-  displayPhones(phones)
+  displayPhones(phones, isShowAll)
 }
 
 
 
-const displayPhones = phones => {
+const displayPhones = (phones, isShowAll) => {
   // console.log(phones);
- 
+
   // step-1: hold the container
   const phoneContainer = document.getElementById('phone-container');
 
@@ -19,15 +19,17 @@ const displayPhones = phones => {
 
   // display show all btn if there are more than 12 phones.
   const searchAllBtn = document.getElementById('show-all-search')
-  if(phones.length>12){
+  if (phones.length > 12 && !isShowAll) {
     searchAllBtn.classList.remove('hidden');
   }
-  else{
+  else {
     searchAllBtn.classList.add('hidden');
   }
 
-   // display only first 12 phones
-   phones = phones.slice(0, 12);
+  // display only first 12 phones if show all doesn't exist
+  if (!isShowAll) {
+    phones = phones.slice(0, 12);
+  }
 
   phones.forEach(phone => {
     console.log(phone);
@@ -42,8 +44,8 @@ const displayPhones = phones => {
           <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-              <button class="btn btn-primary">Buy Now</button>
+            <div class="card-actions justify-center">
+              <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
             </div>
           </div>
     `
@@ -54,13 +56,13 @@ const displayPhones = phones => {
 }
 
 // handle the search btn
-const handleButton = () => {
+const handleButton = (isShowAll) => {
   // console.log('Alhamdulillah')
   toggleLoadingSpinner(true);
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
   console.log(searchText);
-  loadPhone(searchText);
+  loadPhone(searchText, isShowAll);
 }
 
 // handle the another search btn
@@ -76,10 +78,50 @@ const handleButton = () => {
 const toggleLoadingSpinner = (isLoading) => {
   const loadingSpinner = document.getElementById('loadingSpinner');
   // loadingSpinner.classList.remove('hidden')
-  if(isLoading){
+  if (isLoading) {
     loadingSpinner.classList.remove('hidden')
   }
-  else{
+  else {
     loadingSpinner.classList.add('hidden')
   }
+}
+
+//handle showAll 
+
+const handleShowAll = () => {
+  handleButton(true)
+}
+
+// handle show details
+
+const handleShowDetail = async (id) => {
+  // console.log('alhamdulillah');
+  // console.log(id);
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+  const data = await res.json()
+  const phone = data.data;
+  showPhoneDetails(phone)
+}
+
+const showPhoneDetails = (phone) => {
+  console.log(phone);
+  const phoneName = document.getElementById('show-detail-phone-name');
+  phoneName.innerText = phone.name;
+
+  const showDetailsContainer = document.getElementById('show-details-container')
+  showDetailsContainer.innerHTML = `
+       <img class="mx-auto" src="${phone?.image}" alt="" />
+       <p><span class="font-bold">Storage:</span>${phone?.mainFeatures?.storage}</p>
+       <p><span class="font-bold">Display Size:</span>${phone?.mainFeatures?.displaySize}</p>
+       <p><span class="font-bold">ChipSet:</span>${phone?.mainFeatures?.chipSet}</p>
+       <p><span class="font-bold">Memory:</span>${phone?.mainFeatures?.memory}</p>
+       <p><span class="font-bold">Slug:</span>${phone?.slug}</p>
+       <p><span class="font-bold"Release Data:</span>${phone?.releaseDate}</p>
+       <p><span class="font-bold">Brand:</span>${phone?.brand}</p>
+       <p><span class="font-bold">GPS:</span>${phone?.others?.GPS || "No GPS"}</p>      
+  
+  `
+
+  // show the modal
+  show_modal_details.showModal()
 }
